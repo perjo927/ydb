5efc99332c576a33ce56cbc71572a42e74d0fed9
perjo927 <perjo927@student.liu.se>
2015-08-01 13:33:33 +0200

    Refactored footer

---
+++ ydb/client/views/app/footer/footer.template.js
@@ -0,0 +1,10 @@
+Template.footer.events({
+    "submit form": function (event, template) {
+        event.preventDefault();
+
+        console.debug("Send");
+        // TODO: App parse
+
+        // TODO: Email.send(), assets
+    }
+});
--- ydb/server/main.js
+++ ydb/server/main.js
@@ -1,5 +1,19 @@
 // TODO: ES6
+var createAdminUser = function () {
+    //var admin = Server.Methods.ParseAssets("admin");
+    //
+    //try {
+    //    Accounts.createUser({
+    //        "username": admin["username"],
+    //        "email": admin["email"], // TODO: real email
+    //        "password": admin["password"]
+    //    });
+    //} catch (err) {
+    //    //console.log(err);
+    //}
+};
 
 Meteor.startup(function () {
+    //var emailAccount = ParseAssets("emailAccount");
 
 });
