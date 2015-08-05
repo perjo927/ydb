7d79aa6b47c17ab6a02e815f6d60fe85d9e18ba8
perjo927 <perjo927@student.liu.se>
2015-08-05 21:31:21 +0200

    Editing work: Work in progress, images fixed

--- ydb/client/views/app/work/components/work_creator.template.js
+++ ydb/client/views/app/work/components/work_creator.template.js
@@ -1,7 +1,6 @@
 Template.work_creator.events({
     'submit form': function (event) {
         event.preventDefault();
-
         var newWork = WorkCreator.createNewWork(event);
         WorkCreator.insertNewWork(newWork);
     }
